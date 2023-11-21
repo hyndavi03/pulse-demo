2a3dee7dc819d5421965b196073bc5789bad7412
Hyndavi <venkatahyndavi@gmail.com>
2023-11-21 17:56:56 +0530

    modify

--- main.tf
+++ main.tf
@@ -1,49 +1,8 @@
 provider "aws" {
-  region = "ap-south-1"  # Change to your desired region
-}
-
-resource "aws_lambda_function" "lambda_function" {
-  count = var.create_lambda_function ? 1 : 0
-
-  function_name    = var.function_name
-  runtime          = var.runtime
-  handler          = var.handler
-  role             = var.iam_role_arn
-  timeout          = 10
-  memory_size      = 256
-  publish          = true
-  reserved_concurrent_executions = var.concurrency
-
-  s3_bucket        = "s3-bucket-for-lambda-demo"
-  s3_key           = "function.zip"
-
-  environment {
-    variables = {
-      KEY1 = "VALUE1"
-      KEY2 = "VALUE2"
-    }
-  }
-
-  tracing_config {
-    mode = "Active"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_lambda_alias" "lambda_alias" {
-  count       = var.create_lambda_function ? 1 : 0
-  name        = "prod"
-  description = "Production environment"
-  function_name = aws_lambda_function.lambda_function[0].function_name
-  function_version = aws_lambda_function.lambda_function[0].version
+  region = "ap-south-1"
 }
 
 resource "aws_iam_role" "lambda_role" {
-  count = var.create_lambda_function ? 1 : 0
-
   name = "lambda-exec-role"
 
   assume_role_policy = jsonencode({
@@ -63,8 +22,37 @@ resource "aws_iam_role" "lambda_role" {
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   count = var.create_lambda_function ? 1 : 0
 
-  policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role        = aws_iam_role.lambda_role[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role[0].name
 }
 
+resource "aws_lambda_function" "lambda_function" {
+  count = var.create_lambda_function ? 1 : 0
 
+  function_name    = var.function_name
+  runtime          = var.runtime
+  handler          = var.handler
+  role             = aws_iam_role.lambda_role[0].arn
+  timeout          = 10
+  memory_size      = 256
+  publish          = true
+  reserved_concurrent_executions = var.concurrency
+
+  s3_bucket = "s3-bucket-for-lambda-demo"
+  s3_key    = "function.zip"
+
+  environment {
+    variables = {
+      KEY1 = "VALUE1"
+      KEY2 = "VALUE2"
+    }
+  }
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
